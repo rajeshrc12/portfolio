9d6aa953fc16a5dc24385fa0795730ac65f316ad
rajeshrc12 <rajesh.charhajari@gmail.com>
2024-05-06 19:08:07 +0530

    extra code removed

--- src/index.js
+++ src/index.js
@@ -2,22 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createHashRouter, RouterProvider } from "react-router-dom";
-import Index1 from "./portfolio1/Index1";
-import Index2 from "./portfolio2/Index2";
-const router = createHashRouter([
-  {
-    path: "/app",
-    element: <App />,
-  },
-  {
-    path: "/",
-    element: <Index1 />,
-  },
-  {
-    path: "/new",
-    element: <Index2 />,
-  },
-]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(<App />);
